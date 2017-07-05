9d5e80e06098f9e18368686f65d2e1497dea9367
ויסנשטרן <naftaly276@gmail.com>
2017-07-06 00:18:14 +0300

    Add project files.

---
+++ WeatherApp/WeatherApp/Models/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    class City
+    {
+        public City(string cityN,string cityCoor)
+        {
+            CityName = cityN;
+            CityCoordinate = cityCoor;
+        }
+        public string CityName { get; set; }
+        public string CityCoordinate { get; set; }
+    }
+}
---
+++ WeatherApp/WeatherApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _url = "https://api.darksky.net/forecast/8585a7719c87d730bfb90432ebf08832/{0},{1}";
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _mainText;
+      //  private readonly List<city> _dataSource;
+
+        public string MainText
+        {
+            get { return _mainText; }
+            set
+            {
+                _mainText = value;
+                OnPropertyChanged("MainText");
+            }
+        }
+
+        public MainViewModel()
+        {
+            MainText = "Loading...";
+
+           // _dataSource=new List<city>
+            var client = new HttpClient();
+
+            Task.Run(async() =>
+            {
+                try
+                {
+                    var result = await client.GetStringAsync(string.Format(_url, "38.9072", "77.0369")); // Get Json string
+                    //var model = JsonConvert.DeserializeObject(result); // Turn Json into object
+                    var model = JsonConvert.DeserializeObject<RootObject>(result); // Turn Json into specific type
+                    MainText = model.currently.temperature.ToString();
+                }
+                catch(Exception ex)
+                {
+                    // log excepion
+                }
+            });            
+        }
+
+        public void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
